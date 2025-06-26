190389a0829878bb292318d451c95906a5488a6a
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2025-06-26 13:50:20 +0100

    ftest

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Artisan;
 
+
+Schedule::command('inspire')->everyMinute();
+
 //Schedule::command('app:failed-payment-email')->daily()->at('2:00');
 Schedule::command('app:failed-payment-email')->everyFiveMinutes();
