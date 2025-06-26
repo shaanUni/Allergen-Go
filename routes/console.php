85b58e00fe33df0f2a1888acf2d8abd9d6b65c8d
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2025-06-26 13:54:39 +0100

    minute

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Schedule::command('inspire')->everyMinute();
 
 //Schedule::command('app:failed-payment-email')->daily()->at('2:00');
 //Schedule::command('app:failed-payment-email')->everyFiveMinutes();
-Schedule::command('app:failed-payment-email')->minute();
+Schedule::command('app:failed-payment-email')->everyMinute();
