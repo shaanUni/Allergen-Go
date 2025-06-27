417a6ba10f1a8672b9d23bd65ed855718a93aa25
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2025-06-27 17:11:16 +0100

    svg fix

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -42,14 +42,20 @@
             <div class="payment-banner">
                 <div class="payment-banner__icon" aria-hidden="true">
                     <!-- Simple “warning” SVG -->
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         viewBox="0 0 16 16"
-                         width="20" height="20" fill="currentColor">
-                        <path d="M8.982 1.566a1.13 1.13 0 0 0-.964 0L.165 13.233c-.457.778.091 1.767.964 1.767h13.742c.873 0 1.421-.99.964-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1-2.002 0 1 1 0 0 1 2.002 0z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"
+                        fill="currentColor" aria-hidden="true">
+                        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.964 
+                       0L.165 13.233c-.457.778.091 1.767.982 
+                       1.767h13.706c.891 0 1.439-.99.982-1.767L8.982 
+                       1.566zM8 5c.535 0 .954.462.9.995l-.35 
+                       3.507a.552.552 0 0 1-1.1 0L7.1 
+                       5.995A.905.905 0 0 1 8 5zm.002 
+                       6a1 1 0 1 1-2.002 0 1 1 0 0 1 2.002 0z" />
                     </svg>
                 </div>
                 <div class="payment-banner__message">
-                    You have a payment that <strong>failed</strong>. If you do not resolve this soon, your account will be locked until you pay.  
+                    You have a payment that <strong>failed</strong>. If you do not resolve this soon, your account will be
+                    locked until you pay.
                     Please go <a href="{{ route('admin.account') }}">here</a> to update your card details.
                 </div>
             </div>
