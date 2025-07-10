c8ceab89b06fd45ac24cb4f09b404bd9849d1878
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2025-07-10 14:20:19 +0100

    TES 2002

--- resources/views/emails/account-created.blade.php
+++ resources/views/emails/account-created.blade.php
@@ -7,15 +7,16 @@
 
 <p>Thanks for signing up with <strong>AllergenGo</strong>! Your free trial is active and will expire on: <strong>{{ $date }}</strong>.</p>
 
-<p>During your trial, you’ll have full access to our platform—designed to help you easily manage and share allergen and dietary information with your customers. We’re here to make providing information simple and dining safer for everyone.</p>
+<p>You’ll have full access to our platform—designed to help you easily manage and share allergen and dietary information with your customers. We’re here to make providing information simple and dining safer for everyone.</p>
+<p>Once the trial is over, billing was start. Head to the account page at anytime to cancel.</p>
 
 <p>
   If you have any questions or need help getting started, don’t hesitate to contact us — we're always happy to assist. 
   <a href="mailto:support@allergengo.com" style="color: #28a745; font-weight: bold; text-decoration: none;">support@allergengo.com</a>
 </p>
 
-<p style="font-family: Arial, sans-serif; font-size: 14px; color: #333;">Helpful links:</p>
-<div style="margin-top: 4px; font-family: Arial, sans-serif; font-size: 14px;">
+<p style="font-family: Arial, sans-serif; color: #333;">Helpful links:</p>
+<div style="margin-top: 4px; font-family: Arial, sans-serif;">
   <a href="{{ route('admin.agreement') }}" 
      style="color: #28a745; text-decoration: none; display: block; margin-bottom: 6px;">
     Restaurant Customer Agreement
