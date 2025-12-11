8897a355ab48cdce2a023e380f04bbbc3d651815
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2025-12-11 17:43:43 +0000

    add length

--- database/migrations/2025_05_05_091555_create_dishes_table.php
+++ database/migrations/2025_05_05_091555_create_dishes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('admin_id')->index();
             $table->foreign('admin_id')->references('id')->on('admins')->onDelete('cascade');
             $table->string('dish_name')->length(255)->index();
-            $table->text('description')->length(255)->nullable()->index(); 
+            $table->string('description')->length(255)->nullable()->index(); 
             $table->string('allergen_string'); 
             $table->decimal('price', 8, 2);           
             $table->timestamps();
