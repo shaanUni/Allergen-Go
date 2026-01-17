6f56ff001c95bbe8b7e509a2f0d8f6aa7121f618
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2026-01-17 09:37:22 +0000

    use delay properly

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -47,7 +47,7 @@ class DashboardController extends Controller
             InitAccountPageInfo::dispatch($admin);
 
             //send welcome email
-            $admin->notify(new accountCreated())->delay(now()->addMinutes(3));
+            $admin->notify((new accountCreated())->delay(now()->addMinutes(3)));
 
             $this->generate();
 
