9343eed1a5f318c6e1da30ca167fda8426c9907b
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2026-01-17 09:50:54 +0000

    logging

--- app/Http/Controllers/Admin/Auth/RegisterController.php
+++ app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -12,6 +12,8 @@ use Illuminate\Notifications\Notifiable;
 use App\Notifications\accountCreated;
 use Carbon\Carbon;
 
+use Log;
+
 class RegisterController extends Controller
 {
     public function showRegisterForm()
@@ -30,7 +32,6 @@ class RegisterController extends Controller
         if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
             return back()->withErrors(['email' => 'Invalid email address.']);
         }
-        
         $admin = Admin::create([
             'name' => $request->name,
             'email' => $request->email,
@@ -38,6 +39,8 @@ class RegisterController extends Controller
         ]);
 
         session(['pending_admin_id' => $admin->id]);
+
+        Log::info('here');        
         
         // Redirect to Stripe Checkout
         return $admin->newSubscription('default', config('services.stripe.price_id')) // 2nd param is price ID
@@ -50,18 +53,24 @@ class RegisterController extends Controller
 
     public function subscriptionSuccess(Request $request)
     {
+        Log::info('now here');        
+
         $adminId = session('pending_admin_id');
-        
+        Log::info("admin id {$adminId}");
         if ($adminId) {
+            Log::info('inside admin id');        
+
             Auth::guard('admin')->loginUsingId($adminId);  
             session()->forget('pending_admin_id');
             session(['new_user' => 'true']);   
             $admin = Admin::where('id', $adminId)->first();        
             $admin->account_delete_date = null;
             $admin->save();
+            Log::info('redirect suii');        
+
             return redirect()->route('admin.dashboard')->with('success', 'Account created and subscription active!');
         }
-    
+        Log::info("still here");
         return redirect()->route('admin.login')->with('error', 'Session expired or invalid.');
     }
 }
