42a3c95c8404b8889c3d15162dd04567cb16f5d6
Shaan Makoon <shaan.makoon@wirebox.co.uk>
2026-02-28 10:02:21 +0000

    move biz logic that needs to be delayed into toMail

--- app/Notifications/accountCreated.php
+++ app/Notifications/accountCreated.php
@@ -29,6 +29,14 @@ class accountCreated extends Notification implements ShouldQueue
      */
     public function via(object $notifiable): array
     {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
         //Get the admins subscription table
         $subscription = $notifiable->subscription('default');
 
@@ -37,19 +45,11 @@ class accountCreated extends Notification implements ShouldQueue
             Log::info('in here all is fine');
             Log::info($subscription->trial_ends_at);
             $this->date = Carbon::parse($subscription->trial_ends_at)->format('F j, Y');
-            return ['mail'];
 
         } else {
             Log::warning("No subscription found for Admin ID: {$notifiable->id}");
-            return ['mail'];
         }
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail(object $notifiable): MailMessage
-    {
+        
         return (new MailMessage)
             ->subject('Account Created')
             ->view('emails.account-created', ['date' => $this->date]);
